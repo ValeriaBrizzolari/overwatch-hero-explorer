0f7583aa462f69d4081d1ad3b12a38af3814c568
Valeria <bosky@live.it>
2026-04-27 10:02:22 +0100

    Update port for deployment

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ import { log } from "console";
 import { perks } from "./data/perks.js";
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
